b1513889be62720071237d5c6a628f20d3c4f06d
shourovr82 <shafinur512@gmail.com>
2023-01-06 22:55:13 +0600

    s

--- src/components/Pages/MostPlayed/MostPlayed.js
+++ src/components/Pages/MostPlayed/MostPlayed.js
@@ -4,7 +4,7 @@ const MostPlayed = () => {
   return (
     <div className='text-green-900 border'>
       <h2>Shafin</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aperiam neque voluptatem nesciunt sed odit ex commodi. Amet, consequatur magni! Ipsum quo autem facilis perferendis neque dignissimos expedita repellendus quasi jkl.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aperiam neque voluptatem nesciunt sed odit ex commodi. Amet, consequatur magni! Ipsum quo autem facilis perferendis neque dignissimos expedita repellendus quasi jk sl.</p>
       <div>
         <p>Now Playing</p>
       </div>
